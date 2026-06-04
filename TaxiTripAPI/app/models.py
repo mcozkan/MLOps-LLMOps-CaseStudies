6b9f58a55b51dff3de6be7975704fbe62e649aab
mcozkan <mcozkan.ds@gmail.com>
2026-06-04 21:19:07 +0300

    project body created mcoz

---
+++ TaxiTripAPI/app/models.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+class Taxitrip(SQLModel, table = True):
+    __tablename__ = "taxitrips"
+    VendorID : Ooptional
+    tpep_pickup_datetime
+    tpep_dropoff_datetime
+    passenger_count
+    trip_distance
+    RatecodeID
+    store_and_fwd_flag
+    PULocationID
+    DOLocationID
+    payment_type
+    fare_amount
+    extra
+    mta_tax
+    tip_amount
+    tolls_amount
+    improvement_surcharge
+    total_amount
+    congestion_surcharge
+    Airport_fee
+    row_id
