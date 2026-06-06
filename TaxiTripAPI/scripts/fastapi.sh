291cacd51bc776c933c941b1ac89402ec1a749de
mcozkan <mcozkan.ds@gmail.com>
2026-06-06 04:05:00 +0300

    updates done mcoz

--- TaxiTripAPI/scripts/fastapi.sh
+++ TaxiTripAPI/scripts/fastapi.sh
@@ -4,9 +4,4 @@ set -e
 
 echo "Starting FastAPI..."
 # start the FastAPI server in the background
-uvicorn app.main:app --reload --port 8001 &
-
-# Wait for the server to start
-sleep 5
-
-echo "FastAPI is running on http://localhost:8001"
+uvicorn app.main:app --reload --port 8001
